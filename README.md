9ce057c14c0653890bf8d04dd453a2d3b3a96cce
Zach Peterson <zachp@redhat.com>
2025-10-28 13:21:05 -0500

    initial

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ansible-ocpv
+
+Collection of playbooks that build an OpenShift Virtualization VM as well as accomplish several common tasks around the VM build like ServiceNow change management and IPAM/DNS tasks via Netbox and IDM.
---
+++ roles/ocpv_vm/files/winrm_https.ps1
@@ -0,0 +1,23 @@
+# Create self signed certificate
+$certParams = @{
+    CertStoreLocation = 'Cert:\LocalMachine\My'
+    DnsName           = $env:COMPUTERNAME
+    NotAfter          = (Get-Date).AddYears(1)
+    Provider          = 'Microsoft Software Key Storage Provider'
+    Subject           = "CN=$env:COMPUTERNAME"
+}
+$cert = New-SelfSignedCertificate @certParams
+
+# Create HTTPS listener
+$httpsParams = @{
+    ResourceURI = 'winrm/config/listener'
+    SelectorSet = @{
+        Transport = "HTTPS"
+        Address   = "*"
+    }
+    ValueSet = @{
+        CertificateThumbprint = $cert.Thumbprint
+        Enabled               = $true
+    }
+}
+New-WSManInstance @httpsParams
